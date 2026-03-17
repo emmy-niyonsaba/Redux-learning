f9562e149b11e625bcce8b003b12688854fdb9c5
Emmy Niyonsaba <emmmyson5ku@gmail.com>
2026-03-17 19:29:35 +0200

    add the profile and Login components

---
+++ src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+      <button>Loign</button>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Profile() {
+  return (
+    <div>
+      <h1>Scholl:</h1>
+      <h1>Name:</h1>
+      <h1>Class </h1>
+    </div>
+  )
+}
+
+export default Profile
